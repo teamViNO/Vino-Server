0217bfbe476a402368edd0a83b5cb8297d4b4344
Juuunyeok <junyeok333@gmail.com>
2024-01-27 23:35:45 +0900

    [Fix] 라우터 수정

--- src/routes/sms.route.cjs
+++ src/routes/sms.route.cjs
@@ -1,5 +1,5 @@
 const express = require('express');
-const {sendVerificationCode, checkVerificationCode} = require('../controllers/sms.controller');
+const {sendVerificationCode, checkVerificationCode} = require('../controllers/sms.controller.cjs');
 const router = express.Router();
 
 router.post('/sendSMS', sendVerificationCode);
