70f9d4f6f7592309af27e4f3abe24badcdc62475
jainefer <jy5849@naver.com>
2024-01-28 22:46:43 +0900

    [fix]주석처리

--- src/controllers/sms.controller.js
+++ src/controllers/sms.controller.js
@@ -3,34 +3,36 @@ import CoolsmsMessageService from "coolsms-node-sdk";
 
 
 
-export const sendVerificationCode=(req, res)=>{
-  const { phone_number } = req.body;
 
-  const messageService= new CoolsmsMessageService(process.env.COOLSMS_KEY, process.env.COOLSMS_SECRET);
 
-  // 인증코드 생성
-  const verificationCode = Math.floor(1000000 + Math.random() * 9000000); // 7자리
+// export const sendVerificationCode=(req, res)=>{
+//   const { phone_number } = req.body;
 
-  // 세션에 인증 코드와 만료 시간 저장
-  req.session.verification_code = {
-    code: verificationCode,
-    expires: Date.now() + 1000 * 60 * 5  // 5분 후 만료
-  };  
+//   const messageService= new CoolsmsMessageService(process.env.COOLSMS_KEY, process.env.COOLSMS_SECRET);
 
-  // 단일 발송 예제
-  messageService.sendOne({
-    to: phone_number,
-    from: process.env.SMS_PHONE,
-    text: '[VI.NO] 인증 코드: ' + verificationCode,
-  }).then(res => console.log(res));
+//   // 인증코드 생성
+//   const verificationCode = Math.floor(1000000 + Math.random() * 9000000); // 7자리
 
-  // 인증 코드를 응답에 포함시켜 반환
-  res.status(200).json({
-    success: true,
-    message: '인증 코드 전송 성공',
-    verificationCode
-  });
-};
+//   // 세션에 인증 코드와 만료 시간 저장
+//   req.session.verification_code = {
+//     code: verificationCode,
+//     expires: Date.now() + 1000 * 60 * 5  // 5분 후 만료
+//   };  
+
+//   // 단일 발송 예제
+//   messageService.sendOne({
+//     to: phone_number,
+//     from: process.env.SMS_PHONE,
+//     text: '[VI.NO] 인증 코드: ' + verificationCode,
+//   }).then(res => console.log(res));
+
+//   // 인증 코드를 응답에 포함시켜 반환
+//   res.status(200).json({
+//     success: true,
+//     message: '인증 코드 전송 성공',
+//     verificationCode
+//   });
+// };
 
 // 인증코드 확인
 export const checkVerificationCode=(req, res)=>{
