a3244d50fe1781266fa375e8a7527a9fee215cd8
jainefer <jy5849@naver.com>
2024-01-28 16:59:12 +0900

    [fix]  처음부터

--- src/routes/sms.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { sendVerificationCode,checkVerificationCode } from '../controllers/sms.controller.js';
-
-export const smsRoute = express.Router();
-
-smsRoute.post('/sendSMS', async(req,res)=>{
-const result =sendVerificationCode(req,res);
-} );
-smsRoute.post('/checkSMS', checkVerificationCode);
