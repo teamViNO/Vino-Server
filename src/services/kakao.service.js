4ed5c603839c3813991ba937deadeed652d53581
Juuunyeok <junyeok333@gmail.com>
2024-02-09 03:08:24 +0900

    [fix] 카카오 red uri 수정

--- src/services/kakao.service.js
+++ src/services/kakao.service.js
@@ -14,7 +14,7 @@ export const getKakaoUserInfo = async function(code) {
             grant_type: 'authorization_code',
             client_id: process.env.KAKAO_CLIENT_ID,
             code,
-            redirect_uri: process.env.KAKAO_RED_URI
+            redirect_uri: 'http://vi-no.site/social-account'
         }
     });
 
