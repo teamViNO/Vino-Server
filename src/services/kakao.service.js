7e660d27966998187a7294d466b4c68f9f78a545
Juuunyeok <junyeok333@gmail.com>
2024-02-09 11:19:24 +0900

    [fix] 소셜 카카오 수정

--- src/services/kakao.service.js
+++ src/services/kakao.service.js
@@ -50,6 +50,7 @@ export const getKakaoUserInfo = async function(code) {
         "content": '이제부터 어떻게 vino를 사용하면 좋을지 소개해드릴게요 :)',
         "type": "notice"
     }
+    
       // // 환영 인사 알림 추가
     await addWelcomeAlarm(alarmdata);
     
