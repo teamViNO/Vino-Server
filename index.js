19f0b80e2bb8b5e4c53fccc3ac39b33a63aa10f7
Juuunyeok <junyeok333@gmail.com>
2024-02-12 01:08:39 +0900

    [fix] cors 수정

--- index.js
+++ index.js
@@ -33,16 +33,8 @@ const app = express();
 // server setting - veiw, static, body-parser etc..
 app.set('port', process.env.PORT || 3000)   // 서버 포트 지정
 
-const whitelist = ["https://www.vi-no.site", "https://vi-no.site"];
-
 const corsOptions = {
-  origin: function (origin, callback) { 
-    if (whitelist.indexOf(origin) !== -1) { // 만일 whitelist 배열에 origin인자가 있을 경우
-      callback(null, true); // cors 허용
-    } else {
-      callback(new Error("Not Allowed Origin!")); // cors 비허용
-    }
-  },
+  origin: ["https://www.vi-no.site", "https://vi-no.site"],
   optionsSuccessStatus: 200,
   credentials: true,// 응답 헤더에 Access-Control-Allow-Credentials 추가
 };
