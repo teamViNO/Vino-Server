b3c291238e00188c19efa5e7269c65e32a7a7dd3
jainefer <jy5849@naver.com>
2024-02-12 16:11:41 +0900

    [자리 위치 변경]

--- index.js
+++ index.js
@@ -50,7 +50,8 @@ app.use('/images',s3Router);
 app.use('/user', userRoute);
 app.use('/user/myPage', myPageRoute);
 app.use('/dummies',dummyRoute);
-app.use('/search',searchRoute);app.use('/category',categoryRoute);
+app.use('/search',searchRoute);
+app.use('/category',categoryRoute);
 
 app.use(kakaoRoute);
 
