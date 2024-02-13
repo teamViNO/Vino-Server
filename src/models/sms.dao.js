338e49282583e02671b719d5a3f4a68905ebb293
Juuunyeok <junyeok333@gmail.com>
2024-02-13 15:02:06 +0900

    [fix] 전화번호 인증 재구현

---
+++ src/models/sms.dao.js
@@ -0,0 +1,28 @@
+import { pool } from "../../config/db.connect.js";
+import { BaseError } from "../../config/error.js";
+import { status } from "../../config/response.status.js";
+
+export const createCode = async (verificationCode) => {
+
+  const [rows] = await pool.query(
+    'INSERT INTO sms (verification_code) VALUES (?)', 
+    [verificationCode]
+  );
+
+  const [idRows] = await pool.query('SELECT LAST_INSERT_ID() as id');
+  const codeId = idRows[0].id;
+
+  return { id: codeId};
+};
+
+export const findCode = async (id) => {
+  const [rows] = await pool.query('SELECT verification_code FROM sms WHERE id = ?',[id]);
+
+  return rows[0];
+}
+
+export const deleteCode = async (id) => {
+  const [rows] = await pool.query('DELETE FROM sms WHERE id = ?',[id]);
+
+  return rows[0];
+}
