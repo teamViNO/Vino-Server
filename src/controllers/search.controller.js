905ec1a61dfd333f5838243d8be7503970f61e8a
jainefer <jy5849@naver.com>
2024-02-13 21:14:07 +0900

    [fix] 검색 api 수정

--- src/controllers/search.controller.js
+++ src/controllers/search.controller.js
@@ -4,17 +4,23 @@ import {viewSearchKeyword,viewSearchTag}from "../services/search.service.js";
 import  jwt  from "jsonwebtoken";
 
 export const vidoeSearchKeyWord= async (req,res)=>{
-    const token = req.headers.authorization.split(' ')[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.id;
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.id;
 
-    console.log("키워드 검색을 요청하셨습니다");
-    const data={
-        "userId":req.userId,
-        "keyword":req.query.keywordName ?? ''
-    };
-    console.log(data);
-    res.send(response(status.SUCCESS,await viewSearchKeyword(data)));
+        console.log("키워드 검색을 요청하셨습니다");
+        const data={
+            "userId":req.userId,
+            "keyword":req.query.keywordName ?? ''
+        };
+        console.log("컨트롤러데이터",data);
+        res.send(response(status.SUCCESS,await viewSearchKeyword(data)));
+    } catch (error) {
+        console.log(error);
+        throw new BaseError(status.VIDEO_NOT_FOUND);
+    }
+    
 }
 
 export const vidoeSearchTag = async (req, res)=>{
