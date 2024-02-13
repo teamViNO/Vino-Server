aa8a1584546d2c41c0f9060351ffd5ce4bff9650
Juuunyeok <junyeok333@gmail.com>
2024-02-13 15:13:00 +0900

    [fix] 카카오 red_uri 원복

--- src/controllers/kakao.controller.js
+++ src/controllers/kakao.controller.js
@@ -3,9 +3,7 @@ import { getKakaoUserInfo } from "../services/kakao.service";
 export const kakaoLogin = async function(req, res) {
   try {
     const code = req.query.code;
-    const clientHost = req.headers['Client-host'];
-    console.log(clientHost);
-    const data = await getKakaoUserInfo(code, clientHost);
+    const data = await getKakaoUserInfo(code);
     return res.status(200).json(data);
   } catch(e) {
       console.log(e);
