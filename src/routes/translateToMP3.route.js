9c9cb488691d004156cb9f5ee8bd56f1cd096bdf
jainefer <jy5849@naver.com>
2024-02-16 18:05:47 +0900

    [fix] api 수정

--- src/routes/translateToMP3.route.js
+++ src/routes/translateToMP3.route.js
@@ -6,5 +6,6 @@ import { speechresult } from '../controllers/summary.controller.js';
 export const translateToMP3 = express.Router();
 
 // YouTube 링크를 처리하는 라우트
-translateToMP3.get('/v=:videoId', processVideo);
+
 translateToMP3.get('/result', speechresult);
+translateToMP3.get('/', processVideo);
